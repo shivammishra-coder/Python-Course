cd7381382188191be1e252d4cc48cd5728951013
shivammishra-coder <shivam.mishra@jadeglobal.com>
2026-02-10 10:09:38 +0530

    Update fizzbuz.py

--- day2/fizzbuz.py
+++ day2/fizzbuz.py
@@ -3,7 +3,7 @@ class Solution:
         answer = []
         
         for i in range(1, n + 1):
-            # Check for divisibility by both 3 and 5 first
+            
             if i % 3 == 0 and i % 5 == 0:
                 answer.append("FizzBuzz")
             elif i % 3 == 0:
@@ -11,7 +11,7 @@ class Solution:
             elif i % 5 == 0:
                 answer.append("Buzz")
             else:
-                # Numbers must be returned as strings
+                
                 answer.append(str(i))
                 
         return answer
