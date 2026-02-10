98918eae166d3b8183686de64da1509cda97d6bd
shivammishra-coder <shivam.mishra@jadeglobal.com>
2026-02-10 10:10:14 +0530

    Update primefactor.py

--- day2/primefactor.py
+++ day2/primefactor.py
@@ -2,23 +2,23 @@ class Solution:
     def primeFac(self, n):
         res = []
         
-        # 1. Check for the smallest prime factor, 2
+        
         if n % 2 == 0:
             res.append(2)
             while n % 2 == 0:
                 n //= 2
         
-        # 2. Check for odd factors from 3 to sqrt(n)
+        
         i = 3
         while i * i <= n:
             if n % i == 0:
                 res.append(i)
-                # Remove all occurrences of this prime factor
+                
                 while n % i == 0:
                     n //= i
             i += 2
         
-        # 3. If n is still > 1, the remaining n is a prime factor
+        
         if n > 1:
             res.append(n)
             
