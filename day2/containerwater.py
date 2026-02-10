dc62945d906a21b570ed5385274f1c59730a2e92
shivammishra-coder <shivam.mishra@jadeglobal.com>
2026-02-10 10:09:16 +0530

    Update containerwater.py

--- day2/containerwater.py
+++ day2/containerwater.py
@@ -5,18 +5,18 @@ class Solution:
         max_water = 0
         
         while left < right:
-            # Calculate the current width
+            
             width = right - left
             
-            # The height of the water is limited by the shorter bar
+            
             current_height = min(height[left], height[right])
             
-            # Update max_water if the current container holds more
+            
             current_area = current_height * width
             if current_area > max_water:
                 max_water = current_area
             
-            # Move the pointer of the shorter bar inward
+            
             if height[left] < height[right]:
                 left += 1
             else:
